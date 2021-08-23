414851379606c396c975ef27565ea1fe1d609c78
Anthony Lubin <anthonylubin@yahoo.co.uk>
2021-08-23 15:15:57 +0100

    added percentage calculation

--- 03-Javascript Fundamentals/calculator project/script.js
+++ 03-Javascript Fundamentals/calculator project/script.js
@@ -1,24 +1,23 @@
-const plusButton = document.getElementById("plus");  //  +
-const equalsButton = document.getElementById("equals");  //  =
-const minusButton = document.getElementById("minus");  //  -
+const plusButton = document.getElementById("plus"); //  +
+const equalsButton = document.getElementById("equals"); //  =
+const minusButton = document.getElementById("minus"); //  -
 const multiplyButton = document.getElementById("times"); //  *
-const divisionButton = document.getElementById("divide");  //  divide
+const divisionButton = document.getElementById("divide"); //  divide
 const decimalButton = document.getElementById("percent"); //  %
 const parenthesesButton = document.getElementById("brackets"); //  ()
 const resetButton = document.getElementById("cancel"); //   cancel (C)
-const backspaceButton = document.getElementById("back");  //  backspace
+const backspaceButton = document.getElementById("back"); //  backspace
 const calcScreen = document.getElementById("output"); //  output screen
-const buttons = document.querySelectorAll(".digit");  // values & numbers printed onto screen
+const buttons = document.querySelectorAll(".digit"); // values & numbers printed onto screen
 
-//  grab number pressed and print to screen
+//  grab number pressed, add event listener and print to screen
 
 const buttonPress = buttons.forEach((digit) => {
-  digit.addEventListener("click", (event) => {
-      const grabValue = event.target.innerText;
-      const value = document.createTextNode(grabValue);
-      calcScreen.appendChild(value); 
-  })
-  // console.log(number);  // grabs and logs each element in nodelist (array)
+  digit.addEventListener("click", () => {
+    const grabValue = digit.innerText;
+    const value = document.createTextNode(grabValue);
+    calcScreen.appendChild(value);
+  });
 });
 
 //  clear screen
@@ -36,55 +35,95 @@ const backspace = backspaceButton.addEventListener("click", () => {
 // addition calculation
 
 const plusCalculation = equalsButton.addEventListener("click", () => {
-
-if (calcScreen.innerText.includes("+")) {
-const outputToArray = calcScreen.innerText.split("+");
-const convertToNumber = outputToArray.map(number => Number(number));
-const addCalculation = convertToNumber.reduce((total, currentValue) => {
-  return total + currentValue;
-});
-calcScreen.innerText = addCalculation;
-}
+  if (
+    calcScreen.innerText.includes("+") &&
+    !calcScreen.innerText.endsWith("%")
+  ) {
+    const outputToArray = calcScreen.innerText.split("+");
+    const convertToNumber = outputToArray.map((number) => Number(number));
+    const addCalculation = convertToNumber.reduce((total, currentValue) => {
+      return total + currentValue;
+    });
+    calcScreen.innerText = addCalculation;
+  }
 });
 
 // subtraction calculation
 
 const minusCalculation = equalsButton.addEventListener("click", () => {
-
-  if (calcScreen.innerText.includes("-")) {
-  const outputToArray = calcScreen.innerText.split("-");
-  const convertToNumber = outputToArray.map(number => Number(number));
-  const subCalculation = convertToNumber.reduce((total, currentValue) => {
-    return total - currentValue;
-  });
-  calcScreen.innerText = subCalculation;
+  if (
+    calcScreen.innerText.includes("-") &&
+    !calcScreen.innerText.endsWith("%")
+  ) {
+    const outputToArray = calcScreen.innerText.split("-");
+    const convertToNumber = outputToArray.map((number) => Number(number));
+    const subCalculation = convertToNumber.reduce((total, currentValue) => {
+      return total - currentValue;
+    });
+    calcScreen.innerText = subCalculation;
   }
-  });
+});
 
-  //  multiplication calculation
+//  multiplication calculation
 
-  const multiplyCalculation = equalsButton.addEventListener("click", () => {
-
-    if (calcScreen.innerText.includes("x")) {
+const multiplyCalculation = equalsButton.addEventListener("click", () => {
+  if (
+    calcScreen.innerText.includes("x") &&
+    !calcScreen.innerText.endsWith("%")
+  ) {
     const outputToArray = calcScreen.innerText.split("x");
-    const convertToNumber = outputToArray.map(number => Number(number));
+    const convertToNumber = outputToArray.map((number) => Number(number));
     const timesCalculation = convertToNumber.reduce((total, currentValue) => {
       return total * currentValue;
-    },);
-    calcScreen.innerText = timesCalculation;
-    }
     });
+    calcScreen.innerText = timesCalculation;
+  }
+});
 
-    //  division calculation
+//  division calculation
 
-    const divisionCalculation = equalsButton.addEventListener("click", () => {
+const divisionCalculation = equalsButton.addEventListener("click", () => {
+  if (
+    calcScreen.innerText.includes("/") &&
+    !calcScreen.innerText.endsWith("%")
+  ) {
+    const outputToArray = calcScreen.innerText.split("/");
+    const convertToNumber = outputToArray.map((number) => Number(number));
+    const divideCalculation = convertToNumber.reduce((total, currentValue) => {
+      return total / currentValue;
+    });
+    calcScreen.innerText = divideCalculation;
+  }
+});
 
-      if (calcScreen.innerText.includes("/")) {
-      const outputToArray = calcScreen.innerText.split("/");
-      const convertToNumber = outputToArray.map(number => Number(number));
-      const divideCalculation = convertToNumber.reduce((total, currentValue) => {
-        return total / currentValue;
-      });
-      calcScreen.innerText = divideCalculation;
-      }
-      });
+//  percentage calculation
+
+const calculatePercentage = equalsButton.addEventListener("click", () => {
+  const mathsOperators = ["-", "+", "/", "x"];
+
+  mathsOperators.forEach((symbol) => {
+    if (calcScreen.innerText.includes("%" && symbol)) {
+      const outputScreenToArray = calcScreen.innerText
+        .slice(0, -1)
+        .split(symbol);
+      const convertToNumber = outputScreenToArray.map((number) =>
+        Number(number)
+      );
+      const percentCalculation = convertToNumber.reduce(
+        (total, currentValue) => {
+          switch (symbol) {
+            case "-":
+              return Math.abs((total / (100 / currentValue)) - total);
+            case "+":
+              return Math.abs((total / (100 / currentValue)) + total);
+            case "/":
+              return Math.abs((total / (100 / currentValue)) * 100);
+            case "x":
+              return Math.abs((total/100) * currentValue);
+          }
+        }
+      );
+      calcScreen.innerText = percentCalculation;
+    }
+  });
+});
